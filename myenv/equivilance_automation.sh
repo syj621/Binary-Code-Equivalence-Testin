dc0784e58b7eb026ed77b4c3a152698af7c88df8
chote <chotepong.tangton@gmail.c.om>
2024-01-23 21:22:20 -0600

    w

---
+++ myenv/equivilance_automation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+'''
+Psuedo code for automation
+
+- run python file
+- save the cwe name
+- compile the equivilance.c
+- make a new s2e project and call it the specific cwe
+- cd into it
+- maybe configure the config file
+- run the s2e
+- cd out of it
+- delete binary file
+- repeat
+'''
